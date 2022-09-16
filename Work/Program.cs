8428c5631d3c58b502e34edc846f4cd8aec32fb3
Stepan Sm <stepan_smirnoff@mail.ru>
2022-09-16 23:06:18 +0300

    окончание

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,21 +1,50 @@
 ﻿Console.Clear();
-string[] ArrayOfStrings = new string[] { };
+string[] ArrayStrings = new string[] { };
 Console.Write("Введите количество элементов массива - строк:");
 int n = int.Parse(Console.ReadLine());
-ArrayOfStrings = new string[n];
-for (int i = 0; i < ArrayOfStrings.Length; i++)
+ArrayStrings = new string[n];
+for (int i = 0; i < ArrayStrings.Length; i++)
 {
     Console.Write($" Введите {i + 1} строку: ");
-    ArrayOfStrings[i] = Console.ReadLine();
+    ArrayStrings[i] = Console.ReadLine();
 }
 Console.WriteLine();
 Console.WriteLine("массив введенных данных:");
-Console.WriteLine($"{PrintArray(ArrayOfStrings)}");
+Console.WriteLine($"{PrintArray(ArrayStrings)}");
 Console.WriteLine();
 
 int length = 3;  // задаем парметр длины элемент массива, который согласно заданию озвучен
 
+// функция определения количества элемента массива длина которых меньше какой-то заданной длины length
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
 
+int numbers = CheckArray(ArrayStrings, length); 
+// вводим новую переменную в которую будем присваивать вычесленный элемент массива меньше либо равна 3 символа 
+
+string[] newArrayStrings = new string[numbers]; // создаем новый массив
+NewArray(ArrayStrings, newArrayStrings, length);
+
+// функция формирования нового массива строк
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
 
 // функция вывода массива
 string PrintArray(string[] array)
@@ -30,3 +59,6 @@ string PrintArray(string[] array)
     result += " ]";
     return result;
 }
+
+Console.WriteLine("элементы массива  длина которых меньше либо равна 3 символа:");
+Console.WriteLine($"{PrintArray(newArrayStrings)}");
