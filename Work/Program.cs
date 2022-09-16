9fedbd873b44228b83541b279147b1ffb4fb4fcc
Stepan Sm <stepan_smirnoff@mail.ru>
2022-09-16 20:04:08 +0300

    вывод массива на экран

--- Work/Program.cs
+++ Work/Program.cs
@@ -15,3 +15,18 @@ Console.WriteLine();
 
 int length = 3;  // задаем парметр длины элемент массива, который согласно заданию озвучен
 
+
+
+// функция вывода массива
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
